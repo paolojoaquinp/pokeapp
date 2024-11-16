5c0278a3e4881fe88aab1e152dc76cafa035fa8c
Paolo Joaquin Pinto <paolojoaquinpintoperez@gmail.com>
2024-11-16 10:38:38 -0400

    fix: color app bar

--- lib/features/home_page/presentation/presenter/page/home_page.dart
+++ lib/features/home_page/presentation/presenter/page/home_page.dart
@@ -43,7 +43,7 @@ class _Page extends StatelessWidget {
       child: Scaffold(
         appBar: AppBar(
           leading: IconButton(
-            icon: const Icon(Icons.arrow_back),
+            icon: const Icon(Icons.arrow_back, color: Colors.white,),
             onPressed: () => Navigator.pop(context),
           ),
           backgroundColor: Colors.red,
